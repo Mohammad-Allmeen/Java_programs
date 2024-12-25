d4614eb27eb75a0234c3053a168cedda8f6a18fb
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2024-12-25 07:45:20 +0530

    Recommit

---
+++ Parenthesis_check.java
@@ -0,0 +1,21 @@
+public class Parenthesis_check {
+    public static boolean isValid(String s) {
+        while(s.contains("()") || s.contains("{}")|| s.contains("[]"))
+        {
+       s= s.replace("()", "");
+       s= s.replace("{}", "");
+       s= s.replace("[]", "");
+        }
+        return s.isEmpty();
+            }
+            public static void main(String [] args){
+                String s= "[]{}}";
+                boolean check = isValid(s);
+                if(check){
+                    System.out.println("Valid Parenthesis");
+                }
+                else{
+                    System.out.println("Not a Valid Parenthesis");
+                }
+            }
+}
