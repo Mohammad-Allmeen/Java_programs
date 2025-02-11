06c51c7a941c96c6c28847e14a34c7eb749c4ef6
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2025-02-11 14:33:37 +0530

    origin

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
 
 
-    // HOLLOW RECTANGLE
+// HOLLOW RECTANGLE
      
  public static void hollow_rectangle(){
     /* 
@@ -12,7 +12,7 @@ public class Pattern {
 **********
 */
 
-System.out.println("The hollow rectangle is ");
+System.out.println("HOLLOW RECTANGLE ");
 
 for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5; j++) {
@@ -28,11 +28,10 @@ for (int i = 0; i < 5; i++) {
 
  }
 
-
  // RIGHT ANGLE TRIANGLE
 
  public static void right_angle(){
-    System.out.println("The right angle triangle is");
+    System.out.println("THE RIGHT ANGLE TRIANGLE");
     for (int i = 1; i <=4; i++) {
         for (int j = 1; j <=i; j++) {
             System.out.print("*");
@@ -40,6 +39,8 @@ for (int i = 0; i < 5; i++) {
         System.out.println();
     }
  }
+
+ // THE INVERTED RIGHT ANGLE TRINAGLE 
 
  public static void inverted_right_angle(){
     System.out.println("The Inverted right angle triangle is");
@@ -52,6 +53,7 @@ for (int i = 0; i < 5; i++) {
  }
 
  // RIGHT ANGLE TRIANGLE ROTATED 180 DEGREE 
+
     public static void right_angle_rotated(){
         System.out.println("RIGHT ANGLE TRIANGLE ROTATED 180 DEGREE");
         int n=5;
@@ -66,6 +68,58 @@ for (int i = 0; i < 5; i++) {
         }
     }
 
+// RIGHT ANGLE TRIANGLE WITH NUMBERS
+public static void right_angle_numbers(){
+    System.out.println("THE RIGHT ANGLE TRIANGLE WITH NUMBERS");
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j <=i; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+ }
+
+
+  // THE INVERTED RIGHT ANGLE TRINAGLE WITH NUMBER
+
+  public static void inverted_right_angle_numbers(){
+    System.out.println("THE INVERTED RIGHT ANGLE TRINAGLE WITH NUMBER");
+    for (int i = 5 ; i >=1; i--) {
+        for (int j = 1; j <=i; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+ }
+
+
+ // RIGHT ANGLE TRIANGLE WITH CONTINUING NUMBERS
+public static void right_angle_with_continuing_number(){
+    System.out.println("THE INVERTED RIGHT ANGLE TRINAGLE WITH CONTINUING NUMBER");
+    int k=1;
+for (int i = 1; i <=5; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print(k++ +"  ");
+    }
+    System.out.println();
+}
+}
+
+// RIGHT ANGLE WITH ZERO AND ONE 
+
+public static void right_angle_zero_and_one(){
+    for (int i = 1; i <=5; i++) {
+        for (int j = 1; j <=i; j++) {
+            int sum =i+j;
+            if((i+j)%2==0){
+                System.out.print("1 ");
+            }else{
+                System.out.print("0 ");
+            }
+        }
+        System.out.println();
+    }
+}
 
 
     public static void main(String[] args){
@@ -73,6 +127,9 @@ for (int i = 0; i < 5; i++) {
     right_angle();
     inverted_right_angle();
     right_angle_rotated();
-    
+    right_angle_numbers();
+    inverted_right_angle_numbers();
+    right_angle_with_continuing_number();
+    right_angle_zero_and_one();
     }
 }
