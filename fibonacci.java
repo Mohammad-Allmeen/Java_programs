fceb9d26a945c0e592699b72ecf2858ffd0867eb
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2025-05-13 21:25:00 +0530

    Commit

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,41 @@
 public class fibonacci {
+
+
+  public static void fib(int a, int b, int n){
+    if(n==0){
+      return;
+    }
+
+    int sum=a+b;
+    System.out.print(sum+" ");
+    a=b;
+    b=sum;
+    fib(a,b, n-1);
+    
+  }
     public static void main(String args [])
     {
         int n=10;
       int n1=0;
       int n2=1;
-      System.out.println(n1+" "+n2+ " ");{
+      System.out.print(n1+" "+n2+ " ");{
         for (int i = 0; i <n; i++) {
             int n3 = n1+n2;
-            System.out.println(n3+" ");
+            System.out.print(n3+" ");
             n1=n2;
             n2=n3;
         }
       }
+
+      // By recursive function
+      System.out.println();
+     int a=0, b=1;
+     System.out.print(a+ " "+ b+" ");
+      fib(a, b, 6);
     }
-    
 }
+
+
 /*
  *   int count =10;
         int n1=1;
