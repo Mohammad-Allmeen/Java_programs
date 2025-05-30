c293ddda7c5f7075c56635915874ddfd631cad16
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2025-05-30 10:07:48 +0530

    Commit

--- Linked_List.java
+++ Linked_List.java
@@ -1,5 +1,11 @@
 public class Linked_List {
 Node head;
+private int size;
+
+Linked_List(){
+    this.size=0;
+}
+
     class Node{
         String data;
         Node next; 
@@ -7,14 +13,20 @@ Node head;
         Node(String data){
             this.data= data;
             this.next= null; // as it is new node so it will be null
+            size++;
         }
     }
+
+    // method to add element in the starrting of the linked list
+
 
     public void addFirst(String data){
         Node newNode= new Node(data);
         newNode.next=head;
         head=newNode;
     }
+
+    // method to add element in the last of the linked list
 
     public void addLast(String data){
         Node newNode = new Node(data);
@@ -32,16 +44,62 @@ Node head;
     }
 
 
+// method to delete element from the starting of the linked list
+
+public void deleteFirst(){
+//corner case
+if(head==null){
+    System.out.println("No element in the linked list");
+    return;
+}
+size--;
+head= head.next; // when the head is changed to the next node the first element gets deleted, and this is why we should not change/modify the head use any other variable to traverse the linked list 
+}
+
+// method to delete element from the end of the linked list
+
+public void deleteLast(){
+    //corner case
+if(head==null){
+    System.out.println("No element in the linked list");
+    return;
+}
+
+// Second corner case if there is only one element in the link list
+size--;
+if(head.next==null){
+    head=null;
+    return;
+}
+
+
+Node secondLast= head; // variable to traverse the linked list 
+Node lastNode = head.next;
+
+if(lastNode.next!=null){ // it will travel till the lastNode.next comes null i.e we have reached the last node which has no address for the next element
+    secondLast= secondLast.next;
+    lastNode= lastNode.next;
+}
+
+secondLast.next=null; // you will delete the add stored in the second last which will break the link with the last node 
+  
+}
+
+
     public void printList(){
         if(head==null){
             System.out.println("List is empty");
             return;
         }
         Node currNode= head;
-        while(currNode!=null){
+        while(currNode!=null){ // here you do not use currNode.next as you have to print last element also
             System.out.print(currNode.data+" -> ");
             currNode= currNode.next;
         }
+    }
+
+    public void getsize(){
+        System.out.println("The size of Linked list is "+size);
     }
    public static void main(String [] args){
     Linked_List list= new Linked_List();
@@ -50,5 +108,14 @@ Node head;
      list.addFirst("Good Morning");
      list.addLast("Hii");
      list.printList();
+     System.out.println();
+     list.deleteFirst();
+     list.printList();
+
+     System.out.println();
+     list.deleteLast();
+     list.printList();
+     System.out.println();
+     list.getsize();;
    } 
 }
