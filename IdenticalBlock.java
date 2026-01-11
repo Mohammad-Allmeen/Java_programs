5b1f68e40dabc6c6eef7f794e410279c4949ea6e
Mohammad Allmeen <mohdallmeen6630@gmail.com>
2026-01-11 21:01:09 +0530

    Commit

---
+++ IdenticalBlock.java
@@ -0,0 +1,42 @@
+ /* 
+ 
+ Question- Program to find the identical blocks in the Arrays
+
+ Input - 2,3,3,3,2,2,6,4,4,4,4 
+
+ Output - 3 blocks- 333,22,4444
+
+
+ */
+
+ import java.util.Scanner;
+
+public class IdenticalBlock {
+
+
+    public static int func(int arr[]){
+        
+         int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            int value= arr[i];
+            int freq=0;
+            int j=i;
+
+            while(j<arr.length && arr[j]==value){
+                freq++;
+                j++;
+            }
+          if (freq==value){
+           count++;
+          }
+        }
+        return count;
+}
+public static void main(String[] args) {
+   //System.out.println("Enter the value of the array");
+   int arr[] = {2,3,3,3,2,2,6,4,4,4,4};
+   System.out.println(func(arr)); 
+
+}
+}
+
--- MinimumDivisbleAddition.java
+++ MinimumDivisbleAddition.java
@@ -1,6 +1,28 @@
+/* 
+QUESTION
+
+For a given number N (0 < N <= 100), little Johnny wants to find out the minimum positive integer X divisible by N, where the sum of digits of X is equal to N, and X is not equal to N
+
+Note: If such a number X does not exist then output should be -1.
+
+Input Specification:
+
+input1: An integer N
+
+Output Specification:
+
+Return the minimum positive integer X
+
+Example 1:
+
+input1: 9
+
+Output: 18
+
+*/
+
+
 import java.util.Scanner;
-
-
 
 public class MinimumDivisbleAddition{
 
